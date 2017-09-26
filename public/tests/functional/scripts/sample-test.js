aee00e36032b979c2abc26723f5bd2d11129879f
Ricardo Dias <rpdias@br.ibm.com>
2017-09-26 13:26:23 +0000

    Changing the test for telocoach

--- public/tests/functional/scripts/sample-test.js
+++ public/tests/functional/scripts/sample-test.js
@@ -2,10 +2,10 @@ module.exports = {
   'Sample functional test' : function (browser) {
     browser
       .url('https://telocoach.mybluemix.net/')
-      .waitForElementVisible('input.gsfi', 60000, true,
-        function(){}, 'Waiting for Search Input to become visible')
-      .setValue('input.gsfi', 'Test')
-      .waitForElementPresent('#search-page', 60000)
+      .waitForElementVisible('telobtn', 60000, true,
+        function(){}, 'Waiting for button Salario to become visible')
+      .click('telobtn')
+      .waitForElementPresent('div.badges', 60000)
       .pause(5000)
       .end();
   }
