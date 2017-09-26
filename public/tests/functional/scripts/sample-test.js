1b746d1b9478a665cc891c62cd7c813bf81dd1f0
Ricardo Dias <rpdias@br.ibm.com>
2017-09-26 13:39:26 +0000

    Changing ID to #telobtn

--- public/tests/functional/scripts/sample-test.js
+++ public/tests/functional/scripts/sample-test.js
@@ -2,9 +2,9 @@ module.exports = {
   'Sample functional test' : function (browser) {
     browser
       .url('https://telocoach.mybluemix.net/')
-      .waitForElementVisible('telobtn', 60000, true,
+      .waitForElementVisible('#telobtn', 60000, true,
         function(){}, 'Waiting for button Salario to become visible')
-      .click('telobtn')
+      .click('#telobtn')
       .waitForElementPresent('div.badges', 60000)
       .pause(5000)
       .end();
